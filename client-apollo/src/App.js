ed7a4e218f72568f44758da6872d9c673d3e32e0
Mark <ratrateroo@gmail.com>
2021-10-28 16:50:51 +0800

    updated the client

--- client-apollo/src/App.js
+++ client-apollo/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	gql,
+	useMutation,
+} from '@apollo/client';
+import { createUploadLink } from 'apollo-upload-client';
+
+const httpLink = createUploadLink({
+	uri: 'http://localhost:4000/',
+});
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache(),
+});
+
+const UPLOAD_FILE = gql`
+	mutation singleUpload($file: FileUpload!) {
+		singleUpload(file: $file) {
+			filename
+			mimetype
+			encoding
+		}
+	}
+`;
+
+function FileInput() {
+	const [singleUpload] = useMutation(UPLOAD_FILE);
+
+	return (
+		<input
+			type="file"
+			required
+			onChange={({
+				target: {
+					validity,
+					files: [file],
+				},
+			}) => validity.valid && singleUpload({ variables: { file } })}
+		/>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<div>
+				<FileInput />
+			</div>
+		</ApolloProvider>
+	);
 }
 
 export default App;
---
+++ testserver/app.js
@@ -0,0 +1,46 @@
+const { ApolloServer, gql, GraphQLUpload } = require('apollo-server');
+
+const typeDefs = gql`
+	scalar FileUpload
+
+	type File {
+		filename: String!
+		mimetype: String!
+		encoding: String!
+	}
+
+	type Query {
+		uploads: [File]
+	}
+
+	type Mutation {
+		singleUpload(file: FileUpload!): File!
+	}
+`;
+
+const resolvers = {
+	// FileUpload: GraphQLUpload,
+	Query: {
+		uploads: (parent, args) => {},
+	},
+	Mutation: {
+		singleUpload: async (_, { file }) => {
+			const { createReadStream, filename, mimetype, encoding } = await file;
+			const stream = createReadStream();
+
+			// Rest of your code: validate file, save in your DB and static storage
+			console.log(stream);
+			return { filename, mimetype, encoding };
+		},
+	},
+};
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+	console.log(`🚀  Test Server ready at ${url}`);
+});
