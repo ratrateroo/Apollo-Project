42883315b1f50453966f9d229577f733ae8d9537
Mark <ratrateroo@gmail.com>
2021-11-02 17:32:56 +0800

    added read and write to json

--- accessing-node-json/index.js
+++ accessing-node-json/index.js
@@ -1,15 +1,15 @@
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs');
 
 const jsonReader = (filePath, callback) => {
 	readFile(filePath, 'utf-8', (error, fileData) => {
 		if (error) {
-			return callback && callback(err);
+			return callback && callback(error);
 		} else {
 			try {
 				const object = JSON.parse(fileData);
 				return callback && callback(null, object);
 			} catch (error) {
-				return callback && callback(err);
+				return callback && callback(error);
 			}
 		}
 	});
@@ -19,6 +19,35 @@ jsonReader('./files.json', (err, data) => {
 	if (err) {
 		console.log(err);
 	} else {
+		console.log(data.about);
+	}
+});
+
+const newObject = {
+	message: 'hello world',
+	count: 100,
+};
+
+writeFile('./newFile.json', JSON.stringify(newObject, null, 2), (err) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('File successfully written.');
+	}
+});
+
+//read and write
+
+jsonReader('./person.json', (err, data) => {
+	if (err) {
+		console.log(err);
+	} else {
+		data.age += 1;
+		writeFile('./person.json', JSON.stringify(data, null, 2), (err) => {
+			if (err) {
+				console.log(err);
+			}
+		});
 		console.log(data.about);
 	}
 });
