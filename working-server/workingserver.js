33ff0ef2f1872b52231b73a8340b57156b65e17e
Mark <ratrateroo@gmail.com>
2021-11-22 23:26:27 +0800

    updated the server

--- working-server/workingserver.js
+++ working-server/workingserver.js
@@ -1,12 +1,8 @@
 const express = require('express');
-
-//import apollo server
 const { graphqlUploadExpress } = require('graphql-upload');
 
 const apolloserver = require('./apolloserver');
 const createDirectory = require('./createDirectory');
-const readFileNames = require('./readFileNames');
-
 const fileNameReader = require('./readFileNames');
 
 // //testStoreFileSystem function
@@ -15,28 +11,33 @@ const fileNameReader = require('./readFileNames');
 
 //Server Definitions
 const startServer = async () => {
-	console.log('This is the server.');
-	//Start Apollo Server
-	await apolloserver.start();
-	const app = express();
-	app.use(graphqlUploadExpress());
-	apolloserver.applyMiddleware({ app });
-	//Create images folder
-	createDirectory('images');
-	app.use('/freefiles', express.static('public'));
-	app.listen({ port: 4000 }, () => {
-		console.log(
-			`🚀  Server ready at http://localhost:4000/${apolloserver.graphqlPath}`
-		);
-	});
+	try {
+		console.log('This is the server.');
+		//Start Apollo Server
+		await apolloserver.start();
+		const app = express();
+		app.use(graphqlUploadExpress());
+		apolloserver.applyMiddleware({ app });
+		//Create images folder
+		createDirectory('images');
+		//serve public folder for path starting with /freefiles
+		app.use('/freefiles', express.static('public'));
+		app.listen({ port: 4000 }, () => {
+			console.log(
+				`🚀  Server ready at http://localhost:4000/${apolloserver.graphqlPath}`
+			);
+		});
 
-	// storeFileSystem({
-	// 	stream: 'stream',
-	// 	filename: 'filename',
-	// 	mimetype: 'mimetype',
-	// });
+		// storeFileSystem({
+		// 	stream: 'stream',
+		// 	filename: 'filename',
+		// 	mimetype: 'mimetype',
+		// });
 
-	fileNameReader();
+		fileNameReader();
+	} catch (error) {
+		console.log(error);
+	}
 };
 //Starting the Server
 console.log('Server Starting...');
