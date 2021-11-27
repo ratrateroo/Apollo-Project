a10e3ffec5d4ba5d95f04f0b8af4970d353ad70e
Mark <ratrateroo@gmail.com>
2021-11-27 23:52:16 +0800

    adding form for testing

--- client-apollo/src/components/SignUp.js
+++ client-apollo/src/components/SignUp.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUp = () => {
-	const submitHandler = (e) => {
-		e.preventDefault();
-		console.log(e.target.value);
+	const [username, setUsername] = useState();
+	const submitHandler = (event) => {
+		event.preventDefault();
+		setUsername(event.target.value);
+		console.log(username);
 	};
 	return (
 		<div>
-			<form onSubmit={submitHandler}>
+			{/* <form onSubmit={submitHandler}>
 				<label htmlFor="username"></label>
 				<input type="text" placeholder="Enter Username" name="username" />
 				<button type="submit">Sign Up</button>
+			</form> */}
+			{/* <h1>{username.value}</h1> */}
+			<form onSubmit={submitHandler}>
+				<label>
+					Name:
+					<input type="text" />
+				</label>
+				<input type="submit" value="Submit" />
 			</form>
 		</div>
 	);
