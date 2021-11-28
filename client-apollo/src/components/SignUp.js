810481b27029dc9a49a989704ee22c714fc5cd6f
Mark <ratrateroo@gmail.com>
2021-11-28 23:05:45 +0800

    added signup logic on the client

--- client-apollo/src/components/SignUp.js
+++ client-apollo/src/components/SignUp.js
@@ -14,6 +14,55 @@ const SignUp = () => {
 	function submitFormHandler(e) {
 		e.preventDefault();
 		console.log(user);
+		const requestBody = {
+			query: `
+		  mutation createUser(
+			  $username: String!,
+			  
+			  ) {
+		    createUser(userInput: {
+				username: $username,
+				
+				}) {
+		      userId
+			  token
+			  tokenExpiration
+		      
+		    }
+		  }
+		`,
+			variables: {
+				username: user.username,
+			},
+		};
+
+		fetch('http://localhost:4000/graphql', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(requestBody),
+		})
+			.then((res) => {
+				console.log(res);
+				if (res.status !== 200 && res.status !== 201) {
+					throw new Error('Failed!');
+				}
+				return res.json();
+			})
+			.then((resData) => {
+				console.log(resData);
+				// if (resData.data.createUser.token) {
+				// 	auth.login(
+				// 		resData.data.createUser.token,
+				// 		resData.data.createUser.userId,
+				// 		resData.data.createUser.tokenExpiration
+				// 	);
+				// }
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	}
 
 	return (
