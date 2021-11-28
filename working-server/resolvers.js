2b6666df9864e6059f6c3beff3688cacba7fd186
Mark <ratrateroo@gmail.com>
2021-11-28 23:33:15 +0800

    fixed sign up logic

--- working-server/resolvers.js
+++ working-server/resolvers.js
@@ -1,12 +1,41 @@
 const storeUpload = require('./storeUpload');
 const files = require('./files');
+
+const User = require('./models/user');
+
 const resolvers = {
 	Query: {
 		files: () => files,
 	},
 	Mutation: {
 		uploadFile: (parent, { file }) => storeUpload(file),
-		createUser: async (args) => {},
+		createUser: async (parent, { userInput }) => {
+			console.log(userInput.username);
+			try {
+				const existingUser = await User.findOne({
+					username: userInput.username,
+				});
+				if (existingUser) {
+					throw new Error('User exists already.');
+				}
+
+				const user = new User({
+					username: userInput.username,
+
+					profileimage: 'defaultimage',
+				});
+
+				const result = await user.save();
+			} catch (error) {
+				console.log(error);
+			}
+
+			return {
+				userId: 'userId',
+				token: 'token',
+				tokenExpiration: 10,
+			};
+		},
 	},
 };
 
