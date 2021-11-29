37d35ddf895ec37bbedcf732cbd066c96aca47e2
Mark <ratrateroo@gmail.com>
2021-11-29 23:56:47 +0800

    updated apollo client

--- client-apollo/src/components/ApolloClient.js
+++ client-apollo/src/components/ApolloClient.js
@@ -1,13 +1,31 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 import { createUploadLink } from 'apollo-upload-client';
+import { setContext } from '@apollo/client/link/context';
+
+import { AUTH_TOKEN } from '../constants';
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem(AUTH_TOKEN);
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
+});
 
 const link = createUploadLink({
 	uri: 'http://localhost:4000/graphql',
 	// credentials: 'include',
 });
 
+//new client
 export const client = new ApolloClient({
-	link: link,
+	link: authLink.concat(httpLink),
 	cache: new InMemoryCache(),
 });
+
+// export const client = new ApolloClient({
+// 	link: link,
+// 	cache: new InMemoryCache(),
+// });
