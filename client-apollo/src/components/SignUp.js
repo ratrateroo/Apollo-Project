49227c079cd8da490c18eb6b6576579abfb1c810
Mark <ratrateroo@gmail.com>
2022-01-18 23:51:46 +0800

    updated signup page

--- client-apollo/src/components/SignUp.js
+++ client-apollo/src/components/SignUp.js
@@ -1,15 +1,12 @@
 import React, { useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import { LOGGED_IN_USER } from '../constants';
 
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+
 // import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
+
 import Box from '@mui/material/Box';
 
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
