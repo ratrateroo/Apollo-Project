e438c512c4bac26f402b7401d1681ffeb6e0746b
Mark <ratrateroo@gmail.com>
2022-02-12 17:50:59 +0800

    added icon buttons in the navigation

--- client-apollo/src/components/Navigation.js
+++ client-apollo/src/components/Navigation.js
@@ -13,6 +13,9 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
 import { lightGreen } from '@mui/material/colors';
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -98,6 +101,11 @@ const Navigation = () => {
 								component={RouterLink}>
 								Log In User
 							</Button>
+							<IconButton color="inherit">
+								{/* <Brightness7Icon /> */}
+
+								<Brightness4Icon />
+							</IconButton>
 						</Toolbar>
 					</AppBar>
 				</Box>
